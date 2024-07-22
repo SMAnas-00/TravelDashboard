2aaa6e4b0a1d5420e38d6984b40e12bac8533bb5
SMAnas-00 <gouheranas4456@gmail.com>
2024-07-22 11:03:40 +0500

    Changes for Travel

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,13 +1,13 @@
 class AppUrl{
-  static var baseUrl="http://203.130.0.249";
-  static var loginEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=Login";
-  static var logoutEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=MobileLogout";
-  static var callsInfoEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=CallsInfo";
-  static var callsDetailsEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=CallsInfo";
-  static var WTGraphEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=WeeklyTotal";
-  static var WAGraphEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=WeeklyAnswer";
-  static var WMGraphEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=WeeklyMissed";
-  static var WSGraphEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=WeeklySLA";
-  static var MSummaryEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=MonthlyDataSummary";
-  static var MWGraphEndPoint="$baseUrl/ERP/ERP.MobileServices?ActionID=MonthlywiseAnswerCalls";
+  static var baseUrl="http://77.68.16.48";
+  static var loginEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=Login";
+  static var logoutEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=MobileLogout";
+  static var callsInfoEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=CallsInfo";
+  static var callsDetailsEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=CallsInfo";
+  static var WTGraphEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=WeeklyTotal";
+  static var WAGraphEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=WeeklyAnswer";
+  static var WMGraphEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=WeeklyMissed";
+  static var WSGraphEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=WeeklySLA";
+  static var MSummaryEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=MonthlyDataSummary";
+  static var MWGraphEndPoint="$baseUrl/Travel/ERP.MobileServices?ActionID=MonthlywiseAnswerCalls";
 }
